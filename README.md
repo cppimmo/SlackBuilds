4764c38506bd7e5c1d389199875e1f258aaeaf9d
Brian Hoffpauir <bhoffpauir0424@gmail.com>
2021-11-18 23:53:11 -0600

    distrubute.sh changes and add .gitignore

--- README.md
+++
@@ -1,9 +0,0 @@
-# SlackBuilds
-A repository for SlackBuild scripts that I create. May not be entirely *up-to-date*
-SlackBuilds are software package scripts used for Slackware systems.
-Dependencies must be retrieved manually.
-Some packages may be either binary or source packages.
-## Packages Included:
-- xwallpaper (source)
-## Disclaimer
-These are <span color="red"><b>not</b></span> currently official packages; install at your own risk. No <span color="red"><b>warranty</b></span>.
--- distribute.sh
+++ distribute.sh
@@ -2,21 +2,48 @@
 # distribute.sh
 # Use this script to create of tarballs of each SlackBuild folder.
 # I'm using this to easily distribute these on my website.
+
 SLACKBUILDS=( $(echo */) )
 SIZE="${#SLACKBUILDS[@]}"
 COMPRESSION="z"
+BUILD_ALL=false
+CURRENT_DIR=$(pwd)
 
 PROGNAME="$(basename "$0")"
 usage() {
-	echo "$PROGNAME: usage: $PROGNAME [-c,--compression compression flag]"
+	echo "$PROGNAME: usage: $PROGNAME [-c,--compression flag | -b,--build-all]"
 	return
 }
 
 tarball_builds() {
 	for ((i=0; i<"$SIZE"; ++i)); do
-		tar -c"$COMPRESSION"f $(echo "${SLACKBUILDS[i]}" | sed "s/\///").tar.gz \
-			"${SLACKBUILDS[i]}" 
+		tar -c"$COMPRESSION"f "$CURRENT_DIR"/build/$(echo "${SLACKBUILDS[i]}" | sed "s/\///").tar.gz \
+				"${SLACKBUILDS[i]}"
 	done
+	return
+}
+
+build_all() {
+	local current_prgm=
+	mkdir --verbose "$CURRENT_DIR"/build
+	# loop through every directory in CURRENT_DIR and build each into binary packages
+	for ((i=0; i<"$SIZE"; ++i)); do
+		cd "${SLACKBUILDS[i]}"
+		echo "Current Directory is $(basename $(pwd))"
+		current_prgm=$(echo "${SLACKBUILDS[i]}" | sed "s/\///")
+		chmod +x "$current_prgm".info
+	    source "$current_prgm".info
+		if [ "$DOWNLOAD" ]; then
+			wget "$DOWNLOAD"
+		elif [ "$DOWNLOAD_x86_64" ]; then
+			wget "$DOWNLOAD_86_64"
+		fi
+		chmod -x "$current_prgm".info
+		chmod +x "$current_prgm".SlackBuild
+		TMP="$CURRENT_DIR"/build OUTPUT="$CURRENT_DIR"/build ./"$current_prgm".SlackBuild
+		cd ..
+	done
+	rm -f "$CURRENT_DIR"/build/build.*
 	return
 }
 
@@ -25,6 +52,9 @@ while [[ -n "$1" ]]; do
 		-c | --compression)
 			shift
 			COMPRESSION="$1"
+			;;
+		-b | --build-all)
+			BUILD_ALL=true
 			;;
 	    * )
 			usage >&2
@@ -40,8 +70,19 @@ if [[ "$SIZE" = 0 ]]; then
 else
 	echo "The following SlackBuilds were found: "
 	for ((i=0; i<"$SIZE"; ++i)); do
+		if [[ "${SLACKBUILDS[i]}" = "build/" || "${SLACKBUILDS[i]}" = "build" ]]; then
+			# unset "SLACKBUILDS[$i]"
+		fi
 		echo "${SLACKBUILDS[i]}" | sed "s/\///"		
 	done
 	tarball_builds
+fi
+
+if [[ "$BUILD_ALL" = true ]]; then
+	if [ $(id --user) -ne 0  ]; then
+		echo "You must run this script as root to use this option!" >&2
+		exit 1
+	fi
+	build_all
 fi
 
